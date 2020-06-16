e882eeeb3558e58d38dca2c6a451574db3b977d9
qrlk <hi@qrlk.me>
2020-06-16 17:10:29 +0300

    countless changes, mostly edith -> locator

--- server.py
+++ server.py
@@ -5,23 +5,75 @@ import time
 import linecache
 import json
 
-vehicles = {}
+data = {}
 last_clear = time.time()
+
+
 async def handle(request):
+    timer = time.time()
     global last_clear
     global vehicles
-    info = json.loads(urllib.parse.unquote(request.path.replace("/","")))
-    if time.time() - last_clear > 60:
-        last_clear = time.time()
-        for item in list(vehicles):
-            pass
-    for item in info["vehicles"]:
-        print(item)
-        pass
-    return web.Response(text='')
+    info = json.loads(urllib.parse.unquote(request.path.replace("/", "")))
+    if info:
+        timer = time.time()
+        ip = info["info"]["server"]
+        sender = info["info"]["sender"]
+        request_model = info["info"]["request"]
+        allow_occupied = info["info"]["allow_occupied"]
+        allow_unlocked = info["info"]["allow_unlocked"]
+        print(f"[{ip}]: Пришел запрос от {sender}. Он передал информацию о {len(info['vehicles'])} т/с и запросил координаты транспорта с model {request_model}. allow_unlocked: {allow_unlocked}, allow_occupied: {allow_occupied}")
+        if ip not in data:
+            data[ip] = {}
+            data[ip]["vehicles"] = {}
+            data[ip]["senders"] = {}
+        if time.time() - last_clear > 30:
+            print(f"[{ip}]: Запускаю очистку")
+            del_list = []
+            last_clear = time.time()
+            for k, v in data[ip]["vehicles"].items():
+                if int(time.time()) - v["timestamp"] > 25:
+                    print(f"[{ip}]: Vehicle {k} теперь в списке смерти")
+                    del_list.append(k)
+            for item in del_list:
+                del data[ip]["vehicles"][item]
+                print(f"[{ip}]: Транспорт с ID {item} удален")
+            del_list = []
+            for k, v in data[ip]["senders"].items():
+                print(k, v)
+                if int(time.time()) - v > 25:
+                    print(f"[{ip}]: Sender {k} добавлен в список удаления")
+                    del_list.append(k)
+            for item in del_list:
+                del data[ip]["senders"][item]
+                print(f"[{ip}]: Sender {item} удален")
+        data[ip]["senders"][sender] = time.time()
+        for item in info["vehicles"]:
+            item["timestamp"] = time.time()
+            data[ip]["vehicles"][item["id"]] = item
+        print(
+            f"""[{ip}]: Активных сендеров: {len(data[ip]["senders"])}, машин: {len(data[ip]["vehicles"])}""")
+        if request_model != -1:
+            response_cars = []
+            for k, item in data[ip]["vehicles"].items():
+                if item["model"] == request_model:
+                    if item["occupied"] and not allow_occupied:
+                        continue
+                    if not item["locked"] and not allow_unlocked:
+                        continue
+                    response_cars.append(item)
+            if response_cars == []:
+                print(f"[{ip}]: Подготовлен ответ для {sender}. Он хотел model {request_model}, но их не нашлось. (ОТКР: {allow_unlocked}, ЗАН: {allow_occupied}). Обработка заняла: {time.time()-timer} с")
+                return web.Response(text=json.dumps({"result": "ok", "timestamp": time.time(), "active": len(data[ip]["senders"]), "count": len(data[ip]["vehicles"]), "response": "no cars"}))
+            else:
+                print(f"[{ip}]: Подготовлен ответ для {sender}. Он хотел model {request_model}, мы нашли {len(response_cars)} вариантов. (ОТКР: {allow_unlocked}, ЗАН: {allow_occupied}). Обработка заняла: {time.time()-timer} с")
+                return web.Response(text=json.dumps({"result": "ok", "timestamp": time.time(), "active": len(data[ip]["senders"]), "count": len(data[ip]["vehicles"]), "response": response_cars}))
+        else:
+            print(
+                f"[{ip}]: Подготовлен ответ для {sender}. Он не запрашивал машину. Обработка заняла: {time.time()-timer} с")
+            return web.Response(text=json.dumps({"result": "ok", "timestamp": time.time(),  "active": len(data[ip]["senders"]), "count": len(data[ip]["vehicles"])}))
+    return web.Response(text='error')
 
 
 app = web.Application()
 app.router.add_get('/{name}', handle)
-web.run_app(app, host = '0.0.0.0', port=46547)
-
+web.run_app(app, host='0.0.0.0', port=46547)
