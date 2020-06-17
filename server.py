3dc03e9ce9c5d19cb086ffbd4e636ac4a05253df
qrlk <hi@qrlk.me>
2020-06-17 11:38:46 +0300

    Human readable logging request -> response

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from aiohttp import web
+from datetime import datetime
 import urllib.parse
 import time
 import linecache
@@ -21,37 +22,43 @@ async def handle(request):
         request_model = info["info"]["request"]
         allow_occupied = info["info"]["allow_occupied"]
         allow_unlocked = info["info"]["allow_unlocked"]
-        print(f"[{ip}]: РџСЂРёС€РµР» Р·Р°РїСЂРѕСЃ РѕС‚ {sender}. РћРЅ РїРµСЂРµРґР°Р» РёРЅС„РѕСЂРјР°С†РёСЋ Рѕ {len(info['vehicles'])} С‚/СЃ Рё Р·Р°РїСЂРѕСЃРёР» РєРѕРѕСЂРґРёРЅР°С‚С‹ С‚СЂР°РЅСЃРїРѕСЂС‚Р° СЃ model {request_model}. allow_unlocked: {allow_unlocked}, allow_occupied: {allow_occupied}")
+        print(
+            f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S || ')}[{ip}]: Пришел запрос от {sender}. Он передал информацию о {len(info['vehicles'])} т/с и запросил координаты транспорта с model {request_model}. allow_unlocked: {allow_unlocked}, allow_occupied: {allow_occupied}")
         if ip not in data:
             data[ip] = {}
             data[ip]["vehicles"] = {}
             data[ip]["senders"] = {}
-        if time.time() - last_clear > 30:
-            print(f"[{ip}]: Р—Р°РїСѓСЃРєР°СЋ РѕС‡РёСЃС‚РєСѓ")
+        if time.time() - last_clear > 400:
+            print(
+                f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S || ')}[{ip}]: Запускаю очистку")
             del_list = []
             last_clear = time.time()
             for k, v in data[ip]["vehicles"].items():
-                if int(time.time()) - v["timestamp"] > 25:
-                    print(f"[{ip}]: Vehicle {k} С‚РµРїРµСЂСЊ РІ СЃРїРёСЃРєРµ СЃРјРµСЂС‚Рё")
+                if int(time.time()) - v["timestamp"] > 360:
+                    print(
+                        f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S || ')}[{ip}]: Vehicle {k} теперь в списке смерти")
                     del_list.append(k)
             for item in del_list:
                 del data[ip]["vehicles"][item]
-                print(f"[{ip}]: РўСЂР°РЅСЃРїРѕСЂС‚ СЃ ID {item} СѓРґР°Р»РµРЅ")
+                print(
+                    f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S || ')}[{ip}]: Транспорт с ID {item} удален")
             del_list = []
             for k, v in data[ip]["senders"].items():
                 print(k, v)
-                if int(time.time()) - v > 25:
-                    print(f"[{ip}]: Sender {k} РґРѕР±Р°РІР»РµРЅ РІ СЃРїРёСЃРѕРє СѓРґР°Р»РµРЅРёСЏ")
+                if int(time.time()) - v > 360:
+                    print(
+                        f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S || ')}[{ip}]: Sender {k} добавлен в список удаления")
                     del_list.append(k)
             for item in del_list:
                 del data[ip]["senders"][item]
-                print(f"[{ip}]: Sender {item} СѓРґР°Р»РµРЅ")
+                print(
+                    f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S || ')}[{ip}]: Sender {item} удален")
         data[ip]["senders"][sender] = time.time()
         for item in info["vehicles"]:
             item["timestamp"] = time.time()
             data[ip]["vehicles"][item["id"]] = item
         print(
-            f"""[{ip}]: РђРєС‚РёРІРЅС‹С… СЃРµРЅРґРµСЂРѕРІ: {len(data[ip]["senders"])}, РјР°С€РёРЅ: {len(data[ip]["vehicles"])}""")
+            f"""{datetime.now().strftime('%Y-%m-%d %H:%M:%S || ')}[{ip}]: Активных сендеров: {len(data[ip]["senders"])}, машин: {len(data[ip]["vehicles"])}""")
         if request_model != -1:
             response_cars = []
             for k, item in data[ip]["vehicles"].items():
@@ -62,14 +69,16 @@ async def handle(request):
                         continue
                     response_cars.append(item)
             if response_cars == []:
-                print(f"[{ip}]: РџРѕРґРіРѕС‚РѕРІР»РµРЅ РѕС‚РІРµС‚ РґР»СЏ {sender}. РћРЅ С…РѕС‚РµР» model {request_model}, РЅРѕ РёС… РЅРµ РЅР°С€Р»РѕСЃСЊ. (РћРўРљР : {allow_unlocked}, Р—РђРќ: {allow_occupied}). РћР±СЂР°Р±РѕС‚РєР° Р·Р°РЅСЏР»Р°: {time.time()-timer} СЃ")
+                print(
+                    f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S || ')}[{ip}]: Подготовлен ответ для {sender}. Он хотел model {request_model}, но их не нашлось. (ОТКР: {allow_unlocked}, ЗАН: {allow_occupied}). Обработка заняла: {time.time()-timer}:.6f с")
                 return web.Response(text=json.dumps({"result": "ok", "timestamp": time.time(), "active": len(data[ip]["senders"]), "count": len(data[ip]["vehicles"]), "response": "no cars"}))
             else:
-                print(f"[{ip}]: РџРѕРґРіРѕС‚РѕРІР»РµРЅ РѕС‚РІРµС‚ РґР»СЏ {sender}. РћРЅ С…РѕС‚РµР» model {request_model}, РјС‹ РЅР°С€Р»Рё {len(response_cars)} РІР°СЂРёР°РЅС‚РѕРІ. (РћРўРљР : {allow_unlocked}, Р—РђРќ: {allow_occupied}). РћР±СЂР°Р±РѕС‚РєР° Р·Р°РЅСЏР»Р°: {time.time()-timer} СЃ")
+                print(
+                    f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S || ')}[{ip}]: Подготовлен ответ для {sender}. Он хотел model {request_model}, мы нашли {len(response_cars)} вариантов. (ОТКР: {allow_unlocked}, ЗАН: {allow_occupied}). Обработка заняла: {time.time()-timer}:.6f с")
                 return web.Response(text=json.dumps({"result": "ok", "timestamp": time.time(), "active": len(data[ip]["senders"]), "count": len(data[ip]["vehicles"]), "response": response_cars}))
         else:
             print(
-                f"[{ip}]: РџРѕРґРіРѕС‚РѕРІР»РµРЅ РѕС‚РІРµС‚ РґР»СЏ {sender}. РћРЅ РЅРµ Р·Р°РїСЂР°С€РёРІР°Р» РјР°С€РёРЅСѓ. РћР±СЂР°Р±РѕС‚РєР° Р·Р°РЅСЏР»Р°: {time.time()-timer} СЃ")
+                f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S || ')}[{ip}]: Подготовлен ответ для {sender}. Он не запрашивал машину. Обработка заняла: {(time.time()-timer):.6f} с")
             return web.Response(text=json.dumps({"result": "ok", "timestamp": time.time(),  "active": len(data[ip]["senders"]), "count": len(data[ip]["vehicles"])}))
     return web.Response(text='error')
 
